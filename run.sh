5fc6003a01a60a35d8a1fa378d8b4e06f3504d04
Iftach Bar <iftach@riseup.co.il>
2018-11-25 15:13:18 +0200

    fix the build for localstack

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+docker build ${cur_dir} -t localstack/java-maven-node-python -f ${cur_dir}/bin/Dockerfile.base
 docker build ${cur_dir} -t riseup_localstack
 docker-compose -f ${cur_dir}/docker-compose.yml up
